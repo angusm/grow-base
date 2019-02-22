e3ea7475b5853db8e71748f158a20c3b5e3957bc
Angus MacDonald <angusamacdonald@hotmail.com>
2019-02-22 12:06:50 -0600

    Remove tmp dir

--- tmp/js/components/accordion/accordion.js
+++
@@ -1,5 +0,0 @@
-export var counter = function () {
-    for (var i = 1; i <= 100; i++) {
-        console.log(i);
-    }
-};
--- tmp/js/global/main.js
+++
@@ -1,4 +0,0 @@
-import { counter } from "../components/accordion/accordion";
-var foo = new Map([[1, "a"], [2, "b"]]);
-console.log(foo.get(22));
-counter();
